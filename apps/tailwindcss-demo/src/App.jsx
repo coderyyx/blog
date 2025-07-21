bff1afa77c10f8041a169db4fd1ad7032bf408e7
elvan <coderyyx@gmail.com>
2025-07-21 20:05:04 +0800

    feat: Add Tailwind CSS demo with PostCSS configuration and update dependencies

--- apps/tailwindcss-demo/src/App.jsx
+++ apps/tailwindcss-demo/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="content tw-bg-[#fff] tw-text-[20px]">
+    <div className="content tw-text-[20px]">
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
